e9b30262b5653163a150a2b16952f58612d12f84
Yury <43621680+Turuntuntun@users.noreply.github.com>
2019-01-25 21:47:49 +0300

    Add files via upload

--- 1.js
+++ 1.js
@@ -5,7 +5,10 @@ var context1 = canvas1.getContext("2d");
 //var canvas2 = document.getElementById("shop");
 //var context2 = canvas2.getContext("2d");
 
-var value_of_aster = 100, time_of_fire = 70; koef_time1 = [70 , 60 , 50, 40, 30 , 25, 20, 15, 10], koef_time = 0;
+var value_of_aster = 100;
+var time_of_fire = 70;
+var koef_time1 = [70 , 60 , 50, 40, 30 , 25, 20, 15, 10]
+var koef_time = 0;
 //var z = 1;
 var level = 1;
 var aster = [];
@@ -26,7 +29,7 @@ var expl1 = [{
     }];
 var expl = [];
 var fire = [],fire_i = 1, k = [0, 0.4, -0.4, 0.8, -0.8, 1.2, -1,2], k1 = [-5, -4.9, -4.9, -4.8, -4.8, -4.7, -4.7];
-var score = 0, score1 = 0;
+var score = 0, score1 = 0, oldscore = 0;
 var randdx1 = 2, randdx2 = 1, randdy1 = 2, randdy2 = 1;
 
 var imgscore = new Image();
@@ -62,11 +65,9 @@ coins.src = "images/coins3.png";
 var heart = new Image();
 heart.src = "images/hearth.png";
 //Движение 
-canvas.addEventListener("mousemove",function(event){
-	
+canvas.addEventListener("mousemove",function(event){	
      	ship.x = event.offsetX - 1;
-		ship.y = event.offsetY - 1;
-	
+		ship.y = event.offsetY - 1;	
 });
 //Основная функция
 fonimg.onload = function(){
@@ -91,17 +92,17 @@ function buy_fire1(){
 }
 //функция обнуляющая все
 function pregame1(){
-	   time = 15;
-	   $("#but").css("display","none");
-	   $("#shop").css("display","none");
-       $('#wtf').css("cursor","none");
-	   aster.length = 0;
-   	   fire.length = 0;
-   	   randdx1 ++;	
-   	   randdx2 += 0.5;
-   	   randdy1 += 0.1;
-   	   randdy2 += 0.1;
-   	   value_of_aster = arrOfLvl[level - 1];
+    time = 15;
+    $("#but").css("display","none");
+    $("#shop").css("display","none");
+    $('#wtf').css("cursor","none");
+    aster.length = 0;
+    fire.length = 0;
+    randdx1 ++;	
+    randdx2 += 0.5;
+    randdy1 += 0.1;
+    randdy2 += 0.1;
+    value_of_aster = arrOfLvl[level - 1];
 }
 //Функция покупки бомбы
 function buy_bomb(){
@@ -113,48 +114,52 @@ function buy_bomb(){
 }
 //Функция запускающая игру
 function game(){
-   update();
-   render();
-   if(time == 0){
-   	   pause();
-   	   return;
-   }
-   if(ship.life == 0){
-   	ship.x = -100;
-   	ship.y = -100;
-    setTimeout(death,400);
-    return;
-   }
-   requestAnimationFrame(game);
+    update();
+    render();
+    if(time == 0){
+    	score += 5;
+    	context1.clearRect(0, 0, canvas1.width, canvas1.height);
+    	context1.drawImage(clock, 218 * 6, 242 * 1 , 225, 250, 110, 68, 60,60);
+    	render_block()
+   	    pause();
+   	    return;
+    }
+    if(ship.life == 0){
+	   	ship.x = -100;
+	   	ship.y = -100;
+	    setTimeout(death,400);
+	    return;
+    }
+    requestAnimationFrame(game);
 }
 //Кнопка покупки жизней.
 function buy_health(){
-     var cell_health = 10;
-     if(score - cell_health >= 0){
+    var cell_health = 10;
+    if(score - cell_health >= 0){
      	score -= cell_health
      	ship.life++;
-     }
-     context1.clearRect(0, 0, canvas1.width, canvas1.height);
-     context1.drawImage(clock, 218 * 6, 242 * 1 , 225, 250, 110, 68, 60,60);
-     render_block();
+    }
+    context1.clearRect(0, 0, canvas1.width, canvas1.height);
+    context1.drawImage(clock, 218 * 6, 242 * 1 , 225, 250, 110, 68, 60,60);
+    render_block();
 }
 //Покупка абгрейда пушки
 function buy_fire(){
 	var cell_fire = 5;
-     if(score - cell_fire >= 0  && fire_i <= k.length - 1){
+    if(score - cell_fire >= 0  && fire_i <= k.length - 1){
      	score -= cell_fire;
      	fire_i++;
      	context1.clearRect(0, 0, canvas1.width, canvas1.height);
      	context1.drawImage(clock, 218 * 6, 242 * 1 , 225, 250, 110, 68, 60,60);
         render_block();
-     }
+    }
 }
 //Обновление положения
 function update(){
     timer++;
     //Начальные значения камней
     if(timer % value_of_aster == 0){
-       aster.push({
+        aster.push({
 	        x:Math.random() * 550,
         	y:-50,
 	        dx:Math.random() * randdx1 - randdx2,
@@ -165,14 +170,14 @@ function update(){
     //Значение огня
     canvas.addEventListener("click",function(){
         if(timer - timerold > time_of_fire){
-        for(i = 0; i < fire_i; i++){
-    	fire.push({
-    		x:ship.x + 20,
-    		y:ship.y - 15,
-    		dx:k[i],
-    		dy:k1[i]		
-    	});
-}
+	        for(i = 0; i < fire_i; i++){
+	    	fire.push({
+	    		x:ship.x + 20,
+	    		y:ship.y - 15,
+	    		dx:k[i],
+	    		dy:k1[i]		
+	    	});
+		}
     	timerold = timer;
     }
     });    
@@ -215,22 +220,22 @@ function update(){
         	else score -= 5;
         }
         //Проверка столкновений
-        for(j in fire){
-        		if (Math.abs(aster[i].x + 30 - fire[j].x - 10) < 30 && Math.abs(aster[i].y - fire[j].y) < 25){
-        		    score++;
-        		    score1 += level;
-        		    expl.push({
-        		    x:aster[i].x - 25,
-        		    y:aster[i].y - 25, 
-        		    animx: 0, 
-        		    animy: 0 
-        		});
-        		aster[i].del = 1;
-                //Удаление огня
-        		fire.splice(j, 1);
-        		break;	
-        		}
-        }
+    for(j in fire){
+		if (Math.abs(aster[i].x + 30 - fire[j].x - 10) < 30 && Math.abs(aster[i].y - fire[j].y) < 25){
+		    score++;
+		    score1 += level;
+		    expl.push({
+		    x:aster[i].x - 25,
+		    y:aster[i].y - 25, 
+		    animx: 0, 
+		    animy: 0 
+		});
+		aster[i].del = 1;
+        //Удаление огня
+		fire.splice(j, 1);
+		break;	
+		}
+    }
         // Жизни
         if (Math.abs(aster[i].x + 30 - ship.x - 40) < 48 && Math.abs(aster[i].y - ship.y) < 25){
         		    if(ship.life > 0) {
@@ -253,36 +258,36 @@ function update(){
 }
 // Отрисовка всего
 function render(){
-		context.drawImage(fonimg, 0, 0, 600, 600);
-		context.drawImage(shipp, ship.x, ship.y, 60, 30);
-		for (i in fire) context.drawImage(fireimg, fire[i].x, fire[i].y, 20 , 20);
-		for ( i in aster) context.drawImage(astrimg, aster[i].x, aster[i].y , 50 , 50);
-		for( i in expl)
-			context.drawImage(boom, 225 * Math.floor(expl[i].animx),250 * Math.floor(expl[i].animy),	225,250, expl[i].x, expl[i].y, 100,100);   
-        context1.clearRect(0, 0, canvas1.width, canvas1.height);
-        for( i in expl1)
-			context1.drawImage(clock, 218 * Math.floor(expl1[i].animx),242 * Math.floor(expl1[i].animy),	225,250, expl1[i].x, expl1[i].y, 60,60);
-        render_block();
+	context.drawImage(fonimg, 0, 0, 600, 600);
+	context.drawImage(shipp, ship.x, ship.y, 60, 30);
+	for (i in fire) context.drawImage(fireimg, fire[i].x, fire[i].y, 20 , 20);
+	for ( i in aster) context.drawImage(astrimg, aster[i].x, aster[i].y , 50 , 50);
+	for( i in expl)
+		context.drawImage(boom, 225 * Math.floor(expl[i].animx),250 * Math.floor(expl[i].animy),	225,250, expl[i].x, expl[i].y, 100,100);   
+    context1.clearRect(0, 0, canvas1.width, canvas1.height);
+    for( i in expl1)
+		context1.drawImage(clock, 218 * Math.floor(expl1[i].animx),242 * Math.floor(expl1[i].animy),	225,250, expl1[i].x, expl1[i].y, 60,60);
+    render_block();
 }
 //Отрисовка левого блока
 function render_block(){    
-	    context1.fiilStyle = "#ff";
-        context1.font = "30px Arial";
-        context1.fillText(score, 60 , 50);
-        context1.drawImage(coins, 110, 15, 50, 50);
-        context1.fillText( time , 60 , 110);
+    context1.fiilStyle = "#ff";
+    context1.font = "30px Arial";
+    context1.fillText(score, 60 , 50);
+    context1.drawImage(coins, 110, 15, 50, 50);
+    context1.fillText( time , 60 , 110);
 //        context1.drawImage(clock, 5, 73, 50, 50);
-        context1.fillText(ship.life , 60 , 165);
-        context1.drawImage(heart, 110, 135, 52, 40 );
+    context1.fillText(ship.life , 60 , 165);
+    context1.drawImage(heart, 110, 135, 52, 40 );
 }
 //Перерыв между уровнями
 function pause(){
- 	 context.clearRect(0, 0, canvas.width, canvas.height);
-	 context.drawImage(fonimg, 0, 0, 600, 600);
-	 context.fiilStyle = "#ff";
-     context.font = "30px Arial";
-	 $("#wtf").css("cursor","default");
-	 if (level < 10){	
+ 	context.clearRect(0, 0, canvas.width, canvas.height);
+    context.drawImage(fonimg, 0, 0, 600, 600);
+	context.fiilStyle = "#ff";
+    context.font = "30px Arial";
+	$("#wtf").css("cursor","default");
+	if (level < 10){	
         render1();	    	
 	    level++;
 	    } 
